4b2d4d4aa365e820ad6a341ea3d0a710bd677b67
skmtrd <skmtgame23@gmail.com>
2026-03-26 11:15:23 +0900

    Initial Commit

---
+++ CC term/CC_termApp.swift
@@ -0,0 +1,17 @@
+//
+//  CC_termApp.swift
+//  CC term
+//
+//  Created by 坂本蒼哉 on 2026/03/26.
+//
+
+import SwiftUI
+
+@main
+struct CC_termApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CC term/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CC term
+//
+//  Created by 坂本蒼哉 on 2026/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
